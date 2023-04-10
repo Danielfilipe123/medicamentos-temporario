10f2411c0f6644c274f54ebe674de6658f0abb7c
lipef <lipifilipe12345@gmail.com>
2023-04-10 16:59:07 -0300

    codigo-principal

---
+++ projeto-e-pratica1-medicamentos/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inicio</title>
+</head>
+<body>
+    
+<h1>Inicio </h1>
+
+<button><a href="http://localhost:8000/login.php">Logar</a></button>
+<button><a href="http://localhost:8000/cadastro.php">Cadastro</a></button>
+
+</body>
+</html>
